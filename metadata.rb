8afbe37ab625365242433c34fe9b6421dad2c827
Jörg Thalheim <joerg@higgsboson.tk>
2012-10-02 16:20:31 +0200

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer "Cyril Mougel"
+maintainer_email "cyril.mougel@gmail.com"
+license "MIT"
+description "A chef recipe to install oh-my-zsh"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version "0.0.1"
