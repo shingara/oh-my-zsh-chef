0a259f8ea1f158f4a0d7e3f1f0789e8f8fd7de5d
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-02-12 13:18:24 +0100

    Added missing metadata to allow installation from Berkshelf

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,9 @@
 maintainer "Cyril Mougel"
 maintainer_email "cyril.mougel@gmail.com"
+name "oh-my-zsh"
 license "MIT"
 description "A chef recipe to install oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.0.1"
+
+depends 'git'
